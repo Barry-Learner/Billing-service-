8536309cfd1efdb4fece1ec6fc2c7ef45ce1fdb2
BARRY SANOUSSA <93799144+Barry-Learner@users.noreply.github.com>
2022-12-17 19:47:02 +0100

    first comit

--- src/main/java/com/example/billingservice/Feign/CustomerRestClient.java
+++ src/main/java/com/example/billingservice/Feign/CustomerRestClient.java
@@ -2,7 +2,6 @@ package com.example.billingservice.Feign;
 
 
 import com.example.billingservice.model.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
